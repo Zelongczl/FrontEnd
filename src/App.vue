27fe6f519643c0bdbd7668a77883ebec4679810e
Zelongczl <140929250+Zelongczl@users.noreply.github.com>
2023-08-25 19:43:02 +1000

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import HomeView from './views/home/HomeView.vue'
+
+export default (await import('vue')).defineComponent({
+  components: { HomeView },
+  data(){
+    return {
+      
+    }
+  }
+})
+</script>
+
+<style>
+
+</style>
